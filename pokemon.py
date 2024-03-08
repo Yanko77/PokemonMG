9f0b3d0dbb72bdd139875a2e879859a633f1f486
Yanko77 <yanko.youtube@outlook.fr>
2024-03-08 19:36:03 +0100

    Changes by matva

--- pokemon.py
+++ pokemon.py
@@ -99,7 +99,7 @@ class Pokemon:
             if self.level >= self.evolution_level:
                 return Pokemon(self.evolution_name, self.level, self.game, self.is_shiny)
             else:
-                return Pokemon(self.name, self.level, self.is_shiny)
+                return self
 
     def full_heal(self):
         self.health = self.pv
